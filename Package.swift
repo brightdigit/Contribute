1c04394900a8284e453ea477e713654fe04dad5a
Ahmed Shendy <dev.ahmed.shendy@gmail.com>
2023-07-27 00:18:36 +0300

    propose: refactor the way wordpress links are imported

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
   name: "Contribute",
-  platforms: [.macOS(.v12)],
+  platforms: [.macOS(.v13)],
   products: [
     .library(
       name: "Contribute",
