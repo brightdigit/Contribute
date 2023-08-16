3ab06cd810cfc6b6175cf140f5bda739f0282f67
Ahmed Shendy <dev.ahmed.shendy@gmail.com>
2023-08-17 00:03:46 +0300

    revert: Temporary enum to PandocMarkdownGenerator, and keep temporaryDirURL as url extension

--- Sources/Contribute/PandocMarkdownGenerator.swift
+++ Sources/Contribute/PandocMarkdownGenerator.swift
@@ -6,6 +6,23 @@ import Foundation
 
 /// Generates markdown from HTML string using [Pandoc](https://pandoc.org/).
 public struct PandocMarkdownGenerator: MarkdownGenerator {
+  /// A namespace for temporary file operations.
+    public enum Temporary {
+      /// The URL of the temporary directory.
+      private static let temporaryDirURL: URL = .temporaryDirURL
+
+      /// Creates a temporary file from the given content.
+      ///
+      /// - Parameter content: The content of the temporary file.
+      /// - Returns: The URL of the created temporary file.
+      /// - Throws: An error if the temporary file creation fails.
+      public static func file(fromContent content: String) throws -> URL {
+        let temporaryFileURL = temporaryDirURL.appendingPathComponent(UUID().uuidString)
+        try content.write(to: temporaryFileURL, atomically: true, encoding: .utf8)
+        return temporaryFileURL
+      }
+    }
+
   /// The function used for executing shell commands.
   private let shellOut: (String, [String]) throws -> String
 
@@ -23,7 +40,7 @@ public struct PandocMarkdownGenerator: MarkdownGenerator {
   ///   - shellOut: A closure that executes a shell command and returns the output.
   public init(
     shellOut: @escaping (String, [String]) throws -> String,
-    temporaryFile: @escaping (String) throws -> URL = URL.temporaryFile(fromContent:)
+    temporaryFile: @escaping (String) throws -> URL = Temporary.file(fromContent:)
   ) {
     self.shellOut = shellOut
     self.temporaryFile = temporaryFile
