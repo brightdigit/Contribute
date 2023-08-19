0d06b4749d381484c2943e130e6017f444da3a5d
Ahmed Shendy <dev.ahmed.shendy@gmail.com>
2023-08-19 22:16:16 +0300

    fix: 'URLResponse' is unavailable: This type has moved to the FoundationNetworking module.

--- Sources/Contribute/ContributeNetworkManager.swift
+++ Sources/Contribute/ContributeNetworkManager.swift
@@ -1,5 +1,9 @@
 import Foundation
 
+#if canImport(FoundationNetworking)
+  import FoundationNetworking
+#endif
+
 public final class ContributeNetworkManager: URLNetworkManager {
   private let session: URLSession = .shared
 
